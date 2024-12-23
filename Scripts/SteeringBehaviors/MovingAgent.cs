19b33b0760fc564a55ad40f072ba822c72ade088
dante-signal31 <dante.signal31@gmail.com>
2024-12-23 10:14:18 +0100

    Smooth rotations implemented.

--- Scripts/SteeringBehaviors/MovingAgent.cs
+++ Scripts/SteeringBehaviors/MovingAgent.cs
@@ -7,8 +7,19 @@ public partial class MovingAgent : CharacterBody2D
     [ExportCategory("CONFIGURATION:")]
     [Export] private Color _agentColor = new Color(0, 1, 0);
     [Export] private float _maximumSpeed;
+    /// <summary>
+    /// When speed is less than this value, we consider the agent stopped.
+    /// </summary>
     [Export] private float _stopSpeed;
-    [Export] private float _maximumRotationalSpeed;
+    /// <summary>
+    /// The agent maximum rotational speed in degrees.
+    /// </summary>
+    [Export] private float _maximumRotationalDegSpeed;
+    /// <summary>
+    /// Rotation will stop when the difference in degrees between the current rotation and
+    /// current forward vector is less than this value.
+    /// </summary>
+    [Export] private float _stopRotationDegThreshold;
     [Export] private float _maximumAcceleration;
     [Export] private float _maximumDeceleration;
     [Export] private SteeringBehavior _steeringBehavior;
@@ -52,8 +63,15 @@ public partial class MovingAgent : CharacterBody2D
     /// This agent rotation in degrees.
     /// </summary>
     public float Orientation => GlobalRotationDegrees;
+
+    /// <summary>
+    /// This agent forward vector.
+    /// </summary>
+    public Vector2 Forward => GlobalTransform.X;
     
     private SteeringBehaviorArgs _behaviorArgs;
+    private float _maximumRotationSpeedRadNormalized;
+    private float _stopRotationRadThreshold;
 
     private SteeringBehaviorArgs GetSteeringBehaviorArgs()
     {
@@ -62,10 +80,18 @@ public partial class MovingAgent : CharacterBody2D
             Velocity, 
             _maximumSpeed, 
             _stopSpeed,
-            _maximumRotationalSpeed,
+            _maximumRotationalDegSpeed,
             _maximumAcceleration,
             _maximumDeceleration,
             0);
+    }
+
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+        _maximumRotationSpeedRadNormalized =
+            Mathf.DegToRad(_maximumRotationalDegSpeed) / (2 * Mathf.Pi);
+        _stopRotationRadThreshold = Mathf.DegToRad(_stopRotationDegThreshold);
     }
 
     public override void _Ready()
@@ -77,7 +103,7 @@ public partial class MovingAgent : CharacterBody2D
 
     public override void _PhysicsProcess(double delta)
     {
-        // base._PhysicsProcess(delta);
+        base._PhysicsProcess(delta);
         // Update steering behavior args.
         _behaviorArgs.MaximumSpeed = MaximumSpeed;
         _behaviorArgs.CurrentVelocity = Velocity;
@@ -92,8 +118,17 @@ public partial class MovingAgent : CharacterBody2D
         if (steeringOutput.Angular == 0 && Velocity != Vector2.Zero)
         {
             // If no explicit angular steering, we will just look at the direction we
-            // are moving.
-            LookAt(Velocity + GlobalPosition);
+            // are moving, but clamping our rotation by our rotational speed.
+            if (Mathf.Abs(Forward.AngleTo(Velocity)) > _stopRotationRadThreshold)
+            {
+                float newHeading = Mathf.LerpAngle(
+                    Forward.Angle(), 
+                    Velocity.Angle(), 
+                    _maximumRotationSpeedRadNormalized * (float)delta);
+                float rotation = newHeading - Forward.Angle();
+                Vector2 rotatedForward = Forward.Rotated(rotation);
+                LookAt(rotatedForward + GlobalPosition);
+            }
         }
         else
         {
